dc1bffbe840ab3d07cb00e1403f2adf26003212e
Joel Lau <joellau@protonmail.com>
2024-07-15 01:57:47 +0800

    basic form

--- url-shortener-fe/src/app/app.component.spec.ts
+++ url-shortener-fe/src/app/app.component.spec.ts
@@ -1,27 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
+
+  it('should container router outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges()
+
+    const outletElement = fixture.debugElement.query(By.css('router-outlet'))
+    expect(outletElement).toBeTruthy();
+  })
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome url-shortener-fe'
+      "URL Shortener"
     );
   });
 
-  it(`should have as title 'url-shortener-fe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('url-shortener-fe');
-  });
+  // it(`should have as title 'url-shortener-fe'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('url-shortener-fe');
+  // });
 });
--- url-shortener-fe/src/app/app.component.ts
+++ url-shortener-fe/src/app/app.component.ts
@@ -1,14 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { retry } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'url-shortener-fe';
+  formGroup = NewCreateShortUrlForm()
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit(event: Event): void {
+    event.preventDefault()
+
+    if (this.formGroup.valid && this.formGroup.value.original_url) {
+      this.createShortUrl(this.formGroup.value.original_url).pipe(
+        retry(1)
+      ).subscribe({
+        error(err) { console.error(err) },
+        next(response) { console.log(response) }
+      })
+    }
+  }
+
+  private createShortUrl(originalUrl: string) {
+    return this.http.post('api/v1/short-url', { original_url: originalUrl })
+  }
+}
+
+export interface CreateShortUrlRequest {
+  original_url: string
+}
+
+
+export function NewCreateShortUrlForm() {
+  return new FormGroup({
+    original_url: new FormGroup('', [Validators.required])
+  })
 }
