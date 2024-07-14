da61ed0e687f175a7b8ffd030a11acf468ae9a12
Joel Lau <joellau@protonmail.com>
2024-07-15 04:44:47 +0800

    generate resource

--- url-shortener-be/db/migrate/20240714201406_create_short_urls.rb
+++ url-shortener-be/db/migrate/20240714201406_create_short_urls.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
+# :nodoc:
 class CreateShortUrls < ActiveRecord::Migration[7.1]
   def change
     create_table :short_urls do |t|
-      t.string :short_url
+      t.string :short_url, null: true, index: { unique: true }
 
       t.timestamps
     end
