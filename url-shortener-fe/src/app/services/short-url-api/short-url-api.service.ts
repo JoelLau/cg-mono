51ab0f56525f70edfa87fac00e75673fb9b30bd6
Joel Lau <joellau@protonmail.com>
2024-07-15 17:51:21 +0800

    fe: base path env var

--- url-shortener-fe/src/app/services/short-url-api/short-url-api.service.ts
+++ url-shortener-fe/src/app/services/short-url-api/short-url-api.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { retry } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShortUrlApiService {
-  // TODO: configure to be based on env
-  readonly BASE_URL = 'http://localhost:3000';
+  readonly BASE_URL = environment.api_base_path;
 
   constructor(private http: HttpClient) {}
 
---
+++ url-shortener-fe/src/environments/environment.types.ts
@@ -0,0 +1,7 @@
+export interface EnvironmentVariables {
+  // `true` to simulate production environment
+  production: boolean;
+
+  // Represents base path to BE REST API
+  api_base_path: string;
+}
