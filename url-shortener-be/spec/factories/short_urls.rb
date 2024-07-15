d21598ad7c40757ed012f176980f72fdb26cbdf4
Joel Lau <joellau@protonmail.com>
2024-07-15 21:07:38 +0800

    annotate factory

--- url-shortener-be/spec/factories/short_urls.rb
+++ url-shortener-be/spec/factories/short_urls.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: short_urls
+#
+#  id         :bigint           not null, primary key
+#  target_url :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :short_url do
     target_url { 'https://www.example.com' }
